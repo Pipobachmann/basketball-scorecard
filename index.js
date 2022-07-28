17a23fab7df76e39007f6b7d2cf871165d19555e
Pipobachmann <bachmannphilipp94@gmail.com>
2022-07-28 05:12:41 +0200

    Add guest increment function, add reset score function.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let homeScore = document.getElementById("score-home");
-let guestScore = document.getElementById("guest-score");
+let guestScore = document.getElementById("score-guest");
 let homeCount = 0;
 let guestCount = 0;
 
 function incrementHome(n) {
     homeCount += n;
     homeScore.textContent = homeCount;
-
-
-    
 }
-alert(homeScore)
+
+function incrementGuest(n) {
+    guestCount += n;
+    guestScore.textContent = guestCount;
+}
+
+function resetHome(){
+    homeScore.textContent = 0;
+}
+
+function resetGuest(){
+    guestScore.textContent = 0;
+}
